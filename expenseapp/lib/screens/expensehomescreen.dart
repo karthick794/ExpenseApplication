0a1315b60045c1941cd759b9b1cf935c01252815
KARTHICK SAKTHIVEL <54999654+karthick794@users.noreply.github.com>
2024-07-20 14:49:50 +0530

    delete static data

--- expenseapp/lib/screens/expensehomescreen.dart
+++ expenseapp/lib/screens/expensehomescreen.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 
 import 'package:expenseapp/models/expense.dart';
 import 'package:expenseapp/widgets/addexpenseitemwidget.dart';
@@ -33,28 +32,8 @@ void removeexpense(ExpenseModel expense){
     expenseList.remove(expense);
   });
 }
-  final List<ExpenseModel> expenseList = [
-    ExpenseModel(
-        expenseName: "Travel",
-        amount: 20.0,
-        dateTime: DateTime.now(),
-        category: Category.travel),
-    ExpenseModel(
-        expenseName: "Food",
-        amount: 10.0,
-        dateTime: DateTime.now(),
-        category: Category.food),
-    ExpenseModel(
-        expenseName: "Work",
-        amount: 30.0,
-        dateTime: DateTime.now(),
-        category: Category.work),
-    ExpenseModel(
-        expenseName: "Lesiure",
-        amount: 5.0,
-        dateTime: DateTime.now(),
-        category: Category.lesiure),
-  ];
+
+ final List<ExpenseModel> expenseList = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
