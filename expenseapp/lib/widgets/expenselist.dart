bef31cb914f96bae7e32a047bb9ee7e92cede64d
KARTHICK SAKTHIVEL <54999654+karthick794@users.noreply.github.com>
2024-07-20 15:08:38 +0530

    align code

--- expenseapp/lib/widgets/expenselist.dart
+++ expenseapp/lib/widgets/expenselist.dart
@@ -4,16 +4,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class ExpenseListView extends StatelessWidget {
-  const ExpenseListView({super.key,required this.expenseList,required this.removeExpenseList});
+  const ExpenseListView(
+      {super.key, required this.expenseList, required this.removeExpenseList});
   final List<ExpenseModel> expenseList;
   final void Function(ExpenseModel expense) removeExpenseList;
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemCount:expenseList.length,
-    itemBuilder: (ctx,index)=>Dismissible(key: ValueKey(expenseList[index]), onDismissed: (direction) {
-      removeExpenseList(expenseList[index]);
-    },child: ExpenseItem(expenseitem: expenseList[index]))
-    );
+    return ListView.builder(
+        itemCount: expenseList.length,
+        itemBuilder: (ctx, index) => Dismissible(
+            key: ValueKey(expenseList[index]),
+            direction: DismissDirection.endToStart,
+            onDismissed: (direction) {
+              removeExpenseList(expenseList[index]);
+            },
+            child: ExpenseItem(expenseitem: expenseList[index])));
   }
 }
